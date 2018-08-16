b363c4b962aa3d9e38f4867e557dedac5994adb2
Aurimas Vilcinskas <a.vilcinskas@paysera.com>
2018-08-16 14:39:13 +0300

    fix

---
+++ packages/eslint-config-base/rules/style.js
@@ -0,0 +1,41 @@
+module.exports = {
+    rules: {
+        // this option sets a specific tab width for your code
+        // https://eslint.org/docs/rules/indent
+        indent: ['error', 4, {
+            SwitchCase: 1,
+            VariableDeclarator: 1,
+            outerIIFEBody: 1,
+            // MemberExpression: null,
+            FunctionDeclaration: {
+                parameters: 1,
+                body: 1
+            },
+            FunctionExpression: {
+                parameters: 1,
+                body: 1
+            },
+            CallExpression: {
+                arguments: 1
+            },
+            ArrayExpression: 1,
+            ObjectExpression: 1,
+            ImportDeclaration: 1,
+            flatTernaryExpressions: false,
+            ignoredNodes: ['JSXElement', 'JSXElement *']
+        }],
+
+        // specify the maximum length of a line in your program
+        // https://eslint.org/docs/rules/max-len
+        'max-len': ['error', 120, 4, {
+            ignoreUrls: true,
+            ignoreComments: false,
+            ignoreRegExpLiterals: true,
+            ignoreStrings: true,
+            ignoreTemplateLiterals: true,
+        }],
+
+        // https://eslint.org/docs/rules/class-methods-use-this
+        'class-methods-use-this': ['off'],
+    }
+};
