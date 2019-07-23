fe1703142b61d0b1224fee7222cfabdfd1d28f26
Vadim Saveljev <v.saveljev@paysera.lt>
2019-07-23 11:02:30 +0300

    remove rules from eslint config (non-base)

--- packages/eslint-config/rules/react.js
+++ packages/eslint-config/rules/react.js
@@ -15,13 +15,6 @@ module.exports = {
         // Enforce JSX indentation
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent.md
         'react/jsx-indent': ['error', 4],
-
-        // Deprecated as of eslint-plugin-jsx-a11y@6.1.0
-        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
-        'jsx-a11y/label-has-for': 'off',
-
-        'no-plusplus': 'off',
-        'import/prefer-default-export': 'off',
     },
     settings: {
         'import/resolver': {
